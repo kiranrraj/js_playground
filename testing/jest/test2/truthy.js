399bb159f37019e17c787e84729b0a57715aa724
Kiran Raj R <kiranrajvjd@gmail.com>
2022-01-30 22:53:18 +0530

    JavaScript Testing

---
+++ testing/jest/test2/truthy.js
@@ -0,0 +1,19 @@
+const functions = {
+    isTrue: () => {
+        return true;
+    },
+    
+    isFalse: () => {
+        return false;
+    },
+
+    isDefined: () => {
+        return 1;
+    },
+    
+    isUndefined: () => {
+        return undefined;
+    }
+}
+
+module.exports = functions;
---
+++ testing/jest/test2/truthy.test.js
@@ -0,0 +1,23 @@
+const functions = require('./truthy');
+
+test('is the return value true', () => {
+    expect(functions.isTrue()).toBeTruthy(),
+    expect(functions.isTrue()).not.toBeFalsy();
+});
+
+
+test('is the return value false', () => {
+    expect(functions.isFalse()).toBeFalsy(),
+    expect(functions.isFalse()).not.toBeTruthy();
+});
+
+test('is the return value undefined', () => {
+    expect(functions.isUndefined()).toBeUndefined(),
+    expect(functions.isUndefined()).not.toBeDefined();
+});
+
+
+test('is the return value not undefined', () => {
+    expect(functions.isDefined()).not.toBeUndefined(),
+    expect(functions.isDefined()).toBeDefined();
+});

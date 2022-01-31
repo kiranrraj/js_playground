5f8ca16fa22a1226cf02d65291a7c7fae270cdd8
Kiran Raj R <kiranrajvjd@gmail.com>
2022-01-31 12:39:55 +0530

    JavaScript Testing --Jest --Hooks

---
+++ testing/jest/test8_jestHooks/arithmeticOperations.test.js
@@ -0,0 +1,36 @@
+const arithmetic = require('./arithmeticOperations');
+
+describe("Arithmetic Operation Testing", () => {
+
+    beforeAll(() => {
+        console.log("Before All Hooks Called");
+    });
+
+    afterAll(() => {
+        console.log("After All Hooks Called");
+    });
+
+    test.each([[5,6,11], [10, 11, 21], [15, 17, 32]])(
+        `%i + %i equals %i`, (x,y,expected) => {
+            expect(arithmetic.add(x,y)).toBe(expected);
+        }
+    )
+    
+    test.each([[5,6,-1], [10, 4, 6], [15, 7, 8]])(
+        `%i - %i equals %i`, (x,y,expected) => {
+            expect(arithmetic.subtract(x,y)).toBe(expected);
+        }
+    )
+    
+    test.each([[5,2,10], [10, 4, 40], [5, 5, 35]])(
+        `%i * %i equals %i`, (x,y,expected) => {
+            expect(arithmetic.multiply(x,y)).toBe(expected);
+        }
+    )
+    
+    test.each([[15,5,3], [10, 2, 5], [15, 7, 8]])(
+        `%i / %i equals %i`, (x,y,expected) => {
+            expect(arithmetic.divide (x,y)).toBe(expected);
+        }
+    )
+})
